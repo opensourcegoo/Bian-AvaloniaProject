57d16e08ec6a5b938a3805addcbd36e89404bfae
bianyunpeng <bianyunpeng@qq.com>
2025-11-10 23:33:16 +0800

    添加开关按钮使用细节

--- AvaloniaApplication_Start/AvaloniaApplication_Start/Views/BuildIn01View.axaml.cs
+++ AvaloniaApplication_Start/AvaloniaApplication_Start/Views/BuildIn01View.axaml.cs
@@ -18,16 +18,24 @@ public partial class BuildIn01View : UserControl
 
     int i = 1;
     private void rbtn_clck(object? sender, Avalonia.Interactivity.RoutedEventArgs e)
-    {   
+    {
         //this.t2.Text = i++.ToString();
     }
 
     int t1 = 0;
     private void ButtonSpinner_Spin(object? sender, SpinEventArgs e)
     {
-        if(e.Direction == SpinDirection.Increase)
+        if (e.Direction == SpinDirection.Increase)
             this.btnspinner.Content = t1++.ToString();
         else
             this.btnspinner.Content = t1--.ToString();
-    }   
+    }
+
+    private void Button_PointerPressed(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+    {
+    }
+
+    private void Button_PointerPressed_1(object? sender, Avalonia.Input.PointerPressedEventArgs e)
+    {
+    }
 }
